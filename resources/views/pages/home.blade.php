52b155d9152210b18d5fe0198a58fd4ffda2ac91
Unknown <ramaromadhon123@gmail.com>
2020-06-21 12:43:47 +0700

    perbaikan struktur files

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    @extends('layout.head')
+</head>
+
+<body>
+@extends('layout.header')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            
+        </div>
+    </div>
+</div>
+@endsection
+
+@extends('layout.footer')
+
+</body>
+
+</html>
