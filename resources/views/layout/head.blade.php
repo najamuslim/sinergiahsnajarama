ff18bf8123527d88dc83d0c0e357ab4d93065fd5
najamuslim <dinatra.17051@mhs.its.ac.id>
2020-06-23 10:19:28 +0700

    progres

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <style type="text/css">
         .btn-like{
             display: block;
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,5 +1,58 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>Home - Sinergia Homeschooling</title>
+    @extends('layout.head')
+</head>
+
+<div class="top-header bg-info text-white" >
+			<div class="container clearfix align-content:center">
+
+			
+				<div id="et-info">
+									<span class="fa fa-phone"> Phone: 021-57931911 | Mobile : 0822-6041-8532 / 0812-8769-3029</span>
+				
+									<a href="mailto:sinergia.inter@gmail.com"><span class="fas fa-envelope text-white">  sinergia.inter@gmail.com</span></a>
+				
+				<ul class="fa fa-icons">
+
+	<li class="fa fa-facebook">
+		<a href="https://www.facebook.com/profile.php?id=100010998673749" class="icon">
+		</a>
+	</li>
+
+	<li class="fa fa-twitter">
+		<a href="https://twitter.com/sinergiajkt" class="icon">
+		</a>
+	</li>
+
+	<li class="fa fa-instagram">
+		<a href="https://www.instagram.com/sinergiaeducation/" class="icon">
+		</a>
+	</li>
+	<li class="fa fa-youtube">
+		<a href="https://www.youtube.com/watch?v=0a1Xs94Bxtg" class="icon">
+		</a>
+	</li>
+
+	<li class="fa fa-pinterest">
+		<a href="https://id.pinterest.com/hrdsenayan12/" class="icon">
+		</a>
+	</li>
+
+	<li class="fa fa-linkedin">
+		<a href="https://www.linkedin.com/company/sinergia-worldwide-education/" class="icon">
+		</a>
+	</li>
+
+
+</ul>				</div> <!-- #et-info -->
+</div>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand" href="#"><img src=></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -9,14 +62,11 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">About</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
+          Programmes
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="#">Action</a>
@@ -24,6 +74,21 @@
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Admission Sinergia Homeschooling</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">News</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Article</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
     </ul>
   </div>
-
+  </div>
